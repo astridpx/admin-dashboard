abc0185fb0a336487c0027482c95b3b051908cda
Iskyy <iskycruise01@gmail.com>
2023-07-22 20:55:15 +0700

    feat: Created a loader/spinner component

---
+++ src/components/loader/Spinner.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./spinner.css";
+
+export default function Spinner() {
+  return (
+    <>
+      <div className="dot-spinner">
+        <div className="dot-spinner__dot"></div>
+        <div className="dot-spinner__dot"></div>
+        <div className="dot-spinner__dot"></div>
+        <div className="dot-spinner__dot"></div>
+        <div className="dot-spinner__dot"></div>
+        <div className="dot-spinner__dot"></div>
+        <div className="dot-spinner__dot"></div>
+        <div className="dot-spinner__dot"></div>
+      </div>
+    </>
+  );
+}
